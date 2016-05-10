FIX: ppmi formula correcteness. ADD: test the correctness. ADD: main with prints to track the process

sim.py
@@ -1,9 +1,10 @@
-import sys, numpy, re, os
-from collections import Counter
+import json
+import sys, numpy, re, os, math
 
-import math
+import re
 import scipy.sparse as ss
-from nltk.util import ngrams
+from scipy.io import mmwrite, mmread
+from collections import Counter
 
 CLEAN_CORPUS = "clean_corpus"
 DIGIT_REPR = "<!DIGIT!>"
@@ -14,12 +15,23 @@ END_D = re.compile("\s*</text>\s*")
 STRIP = re.compile("['.,:;()+\s\"]+")
 DIGIT = re.compile("[,.\d+]")
 
-#TODO make into class
+def init():
+    index = 0
+    col_dic = {}
+    for word in preprocess(r".\wacky_wiki_corpus_en1.words", relevance_treshold=20000):
+        col_dic[word] = index
+        index += 1
+
+    #col_index_word_map = {col_words_index_map[w]: w for w in col_words_index_map}
+
+    row_dic = get_simlex(r".\simlex_words")
+    #row_index_word_map = {row_words_index_map[w]: w for w in row_words_index_map}
+    return row_dic, col_dic
+
 def preprocess(path_to_corpus, relevance_treshold=None):
     """
     this function create new file to be the preprocessed corpus.
     the preprocessing lowering all words, ignoring digits and punctuation.
-
     :param path_to_corpus: the path to the file contains the corpus.
         the corpus format is as follows:
             Each line corresponds to a word, aside from these special symbols:
@@ -27,7 +39,6 @@ def preprocess(path_to_corpus, relevance_treshold=None):
             </s>: end of sentence
             <text id="">: beginning of document
             </text>: end of document
-
     :return words frequencies
     """
     words_count = Counter()
@@ -91,47 +102,59 @@ def get_simlex(path_to_simlex):
 
     return simlex
 
-
-def create_freq_matrix(corpus_fn, row_dic, col_dic, k):
+def create_freq_matrix(row_dic, col_dic, k, corpus_fn):
     # TODO Add Loop over the corpus
 
-    M = ss.dok_matrix((row_dic.len(), col_dic.len()))
+    M = ss.dok_matrix((len(row_dic), len(col_dic)))
     corpus_f = open(corpus_fn)
-
     for sent in corpus_f:
         sent = k * ". " + sent + k * " ."
         add_to_matrix_gram(sent, row_dic, col_dic, M, k)
 
     return M
 
+def ngrams(input, n):
+  input = input.split(' ')
+  output = []
+  for i in range(len(input)-n+1):
+    output.append(input[i:i+n])
+  return output
+
 
 def add_to_matrix_gram(sent, row_dic, col_dic, M, k):
     n = 2 * k + 1
     middle = k
-    grams = ngrams(sent.split(), n)
+    grams = ngrams(sent, n)
+    #print grams
+    #grams = []
     for gr in grams:
         row_word = gr[middle]
         for word in gr:
-            if (row_word in row_dic and word in col_dic):
+            if row_word in row_dic and word in col_dic:
                 coordinates = (row_dic.get(row_word), col_dic.get(word))
                 M[coordinates] += 1
+                #print M[coordinates]
 
 
 def create_ppmi_matrix(row_dic, col_dic, M):
     # TODO Fix dictionary in every place they occuer
+    Pmatrix = ss.dok_matrix((len(row_dic), len(col_dic)))
     N = 0
-    for i in row_dic:
-        for j in col_dic:
+    for i in row_dic.keys():
+        for j in col_dic.keys():
             coordinates = (row_dic.get(i), col_dic.get(j))
             N += M[coordinates]
+            print(M[coordinates])
 
-    Pmatrix = ss.dok_matrix((row_dic.len(), col_dic.len()))
-    for i in row_dic:
-        add_to_ppmi_matrix(i, col_dic, M, N, Pmatrix)
+    for i in row_dic.keys():
+        add_to_ppmi_matrix(i, row_dic, col_dic, M, N, Pmatrix)
+
+    return Pmatrix
+
 
 def prob_row(cur_rword, row_dic, col_dic, M, N):
     p = 0
-    for j in col_dic:
+    for j in col_dic.keys():
         coordinates = (row_dic.get(cur_rword), col_dic.get(j))
         p += M[coordinates] / N
     return p
@@ -139,59 +162,161 @@ def prob_row(cur_rword, row_dic, col_dic, M, N):
 
 def prob_col(cur_cword, row_dic, col_dic, M, N):
     p = 0
-    for i in row_dic:
+    for i in row_dic.keys():
         coordinates = (row_dic.get(i), col_dic.get(cur_cword))
         p += M[coordinates] / N
     return p
 
 
-def prob_words(row_dic, col_dic, word1, word2, N, M):
+def prob_words(word1, word2, N, M, row_dic, col_dic):
+    sFactor = 2
     coordinates = (row_dic.get(word1), col_dic.get(word2))
-    return M[coordinates] / N
+    upper = M[coordinates] + sFactor
+    lower = len(row_dic) * len(col_dic) * sFactor + N
+    return upper / lower
 
 
-def add_to_ppmi_matrix(row_dic, cur_word, col_dic, M, N, Pmatrix):
-    for i in row_dic:
-        for j in col_dic:
-            coordinates = (row_dic.get(i), col_dic.get(j))
-            probr = prob_row(i, row_dic, col_dic, M, N)
+def add_to_ppmi_matrix(cur_word, row_dic, col_dic, M, N, Pmatrix):
+        for j in col_dic.keys():
+            coordinates = (row_dic.get(cur_word), col_dic.get(j))
+            probr = prob_row(cur_word, row_dic, col_dic, M, N)
             probc = prob_col(j, row_dic, col_dic, M, N)
-            probw = prob_words(i, j, N, M)
-            ppmi = max(0, math.log(probw / (probr * probc), 2))
+            probw = prob_words(cur_word, j, N, M, row_dic, col_dic)
+            if probr == 0 or probc == 0:
+                ppmi = 0
+            else:
+                ppmi = max(0, np.math.log(probw / (probr * probc), 2))
             Pmatrix[coordinates] = ppmi
 
+def calculate_probabilies(row_dic, col_dic, M):
+    N = M.sum()
+    result = ss.dok_matrix((len(row_dic), len(col_dic)))
+    for sim_word in row_dic:
+        for context_word in col_dic:
+            w = row_dic[sim_word]
+            c = col_dic[context_word]
+            result[(w, c)] = M[(w, c)] / N
 
-def get_sim(word1, word2, PMatrix):
-    pass
+    return result
 
-# initialization
-index = 0
-col_words_index_map = {}
-for word in preprocess(r".\toy_corpus", relevance_treshold=20000):
-    col_words_index_map[word] = index
-    index += 1
+def calculate_smoothed_probabilies(row_dic, col_dic, M, factor):
+    N = (factor * len(row_dic) * len(col_dic)) + M.sum()
+    result = ss.dok_matrix((len(row_dic), len(col_dic)))
+    for sim_word in row_dic:
+        for context_word in col_dic:
+            w = row_dic[sim_word]
+            c = col_dic[context_word]
+            result[(w, c)] = (M[(w, c)] + factor) / N
 
-col_index_word_map = {col_words_index_map[w] : w for w in col_words_index_map}
+    return result
 
-row_words_index_map = get_simlex(r".\simlex_words")
-row_index_word_map = {row_words_index_map[w] : w for w in row_words_index_map}
 
-count_matrix = create_freq_matrix(lambda x: x+1, row_words_index_map, col_words_index_map, 2)
-ppmi_matrix = create_ppmi_matrix(row_words_index_map, col_words_index_map, count_matrix)
+def calculate_ppmi(row_dic, col_dic, M):
+    result = ss.dok_matrix((len(row_dic), len(col_dic)))
+    col_probs = [-1 for _ in range(len(col_dic))]
+    for sim_word in row_dic:
+        w = row_dic[sim_word]
+        prob_sim_word = M.getrow(w).sum()
+        for context_word in col_dic:
+            c = col_dic[context_word]
+            if M[(w, c)] == 0:
+                continue
+            if col_probs[c] == -1:
+                col_probs[c] = M.getcol(c).sum()
 
-# evaluation
-GOLD_STD_PAIR_PATT = re.compile("(?P<word1>\w+)\s+(?P<word2>\w+)\s+(?P<POS>[A-Z])\s+(?P<score>\d+\.\d+)\s*")
-def load_simlex_goldstandard(path):
-    test_simlex = {}
-    with open(path, 'r') as gold_std_f:
-        line = gold_std_f.readline()
-        while (line != ""):
-            line = gold_std_f.readline()
-            pair_match = GOLD_STD_PAIR_PATT.match(line)
-            word1 = pair_match.group("word1")
-            word2 = pair_match.group("word2")
+            prob_context_word = col_probs[c]
+            result[(w, c)] = max(0, math.log2(M[(w, c)] / (prob_sim_word * prob_context_word)))
 
-            # any pair of simlex words is mapped into triplet such as (POS, gold_standard score, our score)
-            test_simlex[(word1, word2)] = (pair_match.group("POS"), pair_match.group("score"), get_sim(word1, word2, ppmi_matrix))
+    return result
 
-    return test_simlex
+
+
+def main():
+    # parse command line options
+
+    # row_dic, col_dic = init()
+
+    # preprocess(r".\wacky_wiki_corpus_en1.words", relevance_treshold=20000)
+    with open(r".\rows_indices.json", 'r') as rows_file:
+        row_dic = json.load(rows_file)
+    with open(r".\cols_indices.json", 'r') as cols_file:
+        col_dic = json.load(cols_file)
+
+    # M = create_freq_matrix(row_dic, col_dic, 2, r".\clean_corpus")
+    # mmwrite(r".\freq_matrix_2.mtx", M)
+    # with open(r".\rows_indices.json", 'w+') as rows_file:
+    #     json.dump(row_dic, rows_file)
+    # with open(r".\cols_indices.json", 'w+') as cols_file:
+    #     json.dump(col_dic, cols_file)
+
+    #loading the matrix
+    print("load matrix: context windows of length 5")
+    M = mmread(r".\freq_matrix.mtx").todok()
+    print("matrix loaded\n")
+    if True:
+        print("calculating probabilities")
+        ppmi_5 = calculate_ppmi(row_dic, col_dic, calculate_probabilies(row_dic, col_dic, M))
+        print("recording...")
+        mmwrite(r".\ppmi_5.mtx", ppmi_5)
+
+    if True:
+        print("calculating smoothed probabilities")
+        ppmi_smooth_5 = calculate_ppmi(row_dic, col_dic, calculate_smoothed_probabilies(row_dic, col_dic, M, 2))
+        print("recording...")
+        mmwrite(r".\ppmi_smooth_5.mtx", ppmi_smooth_5)
+
+    print("\n====================================")
+    print("matrix: context windows of length 2")
+    M = mmread(r".\freq_matrix_2.mtx").todok()
+    print("matrix loaded\n")
+    if True:
+        print("calculating smoothed probabilities")
+        ppmi_2 = calculate_ppmi(row_dic, col_dic, calculate_probabilies(row_dic, col_dic, M))
+        print("recording...")
+        mmwrite(r".\ppmi_2.mtx", ppmi_2)
+
+    if True:
+        print("calculating smoothed probabilities")
+        ppmi_smooth_2 = calculate_ppmi(row_dic, col_dic, calculate_smoothed_probabilies(row_dic, col_dic, M, 2))
+        print("recording...")
+        mmwrite(r".\ppmi_smooth_2.mtx", ppmi_smooth_2)
+
+    print("finish")
+
+def test():
+    testM = ss.dok_matrix((4, 5))
+    col_dic = {
+        "computer" : 0,
+        "data" : 1,
+        "pinch" : 2,
+        "result" : 3,
+        "sugar" : 4
+    }
+
+    row_dic = {
+        "apricot" : 0,
+        "pineapple" : 1,
+        "digital" : 2,
+        "information" : 3
+    }
+
+    testM[(0, 2)] = 1
+    testM[(0, 4)] = 1
+    testM[(1, 2)] = 1
+    testM[(1, 4)] = 1
+    testM[(2, 0)] = 2
+    testM[(2, 1)] = 1
+    testM[(2, 3)] = 1
+    testM[(3, 0)] = 1
+    testM[(3, 1)] = 6
+    testM[(3, 3)] = 4
+
+    probM = calculate_smoothed_probabilies(row_dic, col_dic, testM, 2)
+    # print(probM)
+    ppmiM = calculate_ppmi(row_dic, col_dic, probM)
+    print(ppmiM)
+
+# test()
+
+if __name__ == "__main__":
+    main()